d9e8c56112b486cb1c5ba26dd35e8b03f8c15014
asaje379 <salemaffa@gmail.com>
2024-03-26 08:01:15 +0100

    chore(): setting up publish config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,4 @@
-type Greeting = {
+export type Greeting = {
   author: string;
   target: string;
 };
-
-const a: Greeting = { author: 'John', target: 'Sarah' };
-
-console.log(`[${a.author}]: Hi ${a.target}`);
